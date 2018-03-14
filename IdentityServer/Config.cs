32990ba031d3cd63d0e9a7a4eea8d65d26840a28
jennahooper <jmhooper7@gmail.com>
2018-03-14 08:08:20 -0400

    Initial commit

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            List<Client> clients = new List<Client>();
+
+            // Client 1
+            clients.Add(new Client()
+            {
+                ClientId = "Client1",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())
+                }
+            });
+            return clients;
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            List<ApiResource> apiResources = new List<ApiResource>();
+            apiResources.Add(new ApiResource("api1", "First API"));
+
+            return apiResources;
+        }
+    }
+}
